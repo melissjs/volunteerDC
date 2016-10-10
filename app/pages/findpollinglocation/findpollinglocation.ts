0208f8685b3d6306479f47f5582bdfbf26e93acf
Melissa Schwartz <melissjs@gmail.com>
2016-10-10 14:59:33 -0700

    changes to list

--- app/pages/findpollinglocation/findpollinglocation.ts
+++ app/pages/findpollinglocation/findpollinglocation.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { PollingStation } from '../../pollingstation.ts';
+import { PollingstationComponent } from '../pollingstationcomponent/pollingstationcomponent';
 import { Volunteer} from '../../volunteer.ts';
 import { Team } from '../../team.ts';
 
@@ -9,7 +10,8 @@ import { Team } from '../../team.ts';
 
 @Component({
   templateUrl: 'build/pages/findpollinglocation/findpollinglocation.html',
-  inputs: ['pollingstation', 'volunteer', 'team']
+  inputs: ['pollingstation', 'volunteer', 'team'],
+  directives: [PollingstationComponent]
 })
 export class FindpollinglocationPage {
 currentVolunteer: Volunteer; 
@@ -62,7 +64,7 @@ this.stations =
         
       },    {
         "precinctNumber": "9001b",
-        "streetAddress": "515 Almont Drive",
+        "streetAddress": "5445 Roxbery Lane",
         "unitNumber": "11",
         "roomNumber": "2",
         "city": "Los Angeles",
@@ -97,7 +99,46 @@ this.stations =
         "totalAnomalyRecords": 0,
         "totalAmendmentRecords": 1
       }],
-         "totalRegisteredVolunteers": 2,
+        "totalRegisteredVolunteers": 2,
+        "totalNeededVolunteers": 4
+      },      {
+        "precinctNumber": "1001b",
+        "streetAddress": "4209 Raymond Road",
+        "unitNumber": "11",
+        "roomNumber": "2",
+        "city": "Marina Del Rey",
+        "state": "California",
+        "zip": 90025,
+        "associatedVolunteerList": [{
+                
+        "fullName":"Melissa Schwartz",
+        "emailAddress":"melissjs@gmail.com",
+        "phoneNumber":"602-524-5453",
+        "age": 35,
+        "sex": "Female",
+        "partyAffiliation": "No Party Preference",    
+        "shifts": "Morning, Evening",
+        "passcode": "Eric help me!",
+        "totalRecords": 6,
+        "totalVoteRecords": 5,
+        "totalAnomalyRecords": 0,
+        "totalAmendmentRecords": 1
+      
+        },      {
+        "fullName":"Eric Hillis",
+        "emailAddress":"eric@hillis.com",
+        "phoneNumber":"310-222-3333",
+        "age": 40,
+        "sex": "Male",
+        "partyAffiliation": "No Party Preference",    
+        "shifts": "Afternoon",
+        "passcode": "Code",
+        "totalRecords": 6,
+        "totalVoteRecords": 5,
+        "totalAnomalyRecords": 0,
+        "totalAmendmentRecords": 1
+      }],
+        "totalRegisteredVolunteers": 2,
         "totalNeededVolunteers": 4
       }]
 
---
+++ app/pages/pollingstationcomponent/pollingstationcomponent.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'pollingstationcomponent',
+  templateUrl: 'build/pages/pollingstationcomponent/pollingstationcomponent.html',
+  inputs: ['passedStations']
+})
+export class PollingstationComponent {
+
+
+}
