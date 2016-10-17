0c7d25b70ba7e5446764a34c5dab6e4ec6114cde
Melissa Schwartz <melissjs@gmail.com>
2016-10-16 23:31:10 -0700

    not sure if I adopted your changes

--- app/pages/temp/temp.ts
+++ app/pages/temp/temp.ts
@@ -46,90 +46,91 @@ export class TempPage {
             this.currentVolunteer = volunteerservice.getNewVolunteer();
         }
 
-        //for Testing only
-        this.currentTempVolunteer = {
-            volunteerKey: 'v5',
-            fullName: 'Raya Hammond',
-            emailAddress: 'email@email.com',
-            exposeEmail: true,
-            phoneNumber: '6024539544',
-            age: 123,
-            sex: 'Female',
-            partyAffiliation: 'Other Party',
-            shifts:'Late Morning, Early Evening, Early Morning, Late Evening', //'Late Morning, Early Evening'
-            passcode: 'passcodestring',
-            associatedPollingStationKey:'ps1', 
-            totalRecords: 0,
-            totalVoteRecords: 0,
-            totalAnomalyRecords: 0,
-            totalAmendmentRecords: 0,
-        } 
+
+//for Testing only
+this.currentTempVolunteer = {
+volunteerKey: 'v5',
+fullName: 'Raya Hammond',
+emailAddress: 'email@email.com',
+exposeEmail: true,
+phoneNumber: '6024539544',
+age: 123,
+sex: 'Female',
+partyAffiliation: 'Other Party',
+shifts:'Late Morning, Early Evening, Early Morning, Late Evening', //'Late Morning, Early Evening'
+passcode: 'passcodestring',
+associatedPollingStationKey:'ps1', 
+totalRecords: 0,
+totalVoteRecords: 0,
+totalAnomalyRecords: 0,
+totalAmendmentRecords: 0,
+} 
 
 
 
-        //form stuff
-        var regExEmail: string = '[A-Za-z0-9._-][A-Za-z0-9._-]*@[A-Za-z0-9._-][A-Za-z0-9._-]*\.[a-zA-Z][a-zA-Z]*';
-        var regExPhone: string = '[2-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]';
-        var regExAge: string = '[1]*[0-9]?[0-9]';
+    //form stuff
+    var regExEmail: string = '[A-Za-z0-9._-][A-Za-z0-9._-]*@[A-Za-z0-9._-][A-Za-z0-9._-]*\.[a-zA-Z][a-zA-Z]*';
+    var regExPhone: string = '[2-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]';
+    var regExAge: string = '[1]*[0-9]?[0-9]';
 
-        this.changeForm = fb.group({  
-            'fullNameCtrl': [this.currentTempVolunteer.fullName, Validators.compose([Validators.required])],
-            'emailAddressCtrl': [this.currentTempVolunteer.emailAddress, Validators.compose([Validators.required, Validators.minLength(4), Validators.pattern(regExEmail)])],
-            'exposeEmailCtrl': [this.currentTempVolunteer.exposeEmail, Validators.required],
-            'phoneNumberCtrl': [this.currentTempVolunteer.phoneNumber, Validators.compose([Validators.required, Validators.minLength(4), Validators.pattern(regExPhone)])],
-            'ageCtrl': [this.currentTempVolunteer.age, Validators.compose([Validators.required, Validators.minLength(2), Validators.pattern(regExAge)])],
-            'sexCtrl': [this.currentTempVolunteer.sex, Validators.required],
-            'partyAffiliationCtrl': [this.currentTempVolunteer.partyAffiliation, Validators.required],
-            'shiftsCtrl': [this.currentTempVolunteer.shifts, Validators.required],
-            'passcodeCtrl': [this.currentTempVolunteer.passcode, Validators.required],
+this.changeForm = fb.group({  
+    'fullNameCtrl': [this.currentTempVolunteer.fullName, Validators.compose([Validators.required])],
+    'emailAddressCtrl': [this.currentTempVolunteer.emailAddress, Validators.compose([Validators.required, Validators.minLength(4), Validators.pattern(regExEmail)])],
+    'exposeEmailCtrl': [this.currentTempVolunteer.exposeEmail, Validators.required],
+    'phoneNumberCtrl': [this.currentTempVolunteer.phoneNumber, Validators.compose([Validators.required, Validators.minLength(4), Validators.pattern(regExPhone)])],
+    'ageCtrl': [this.currentTempVolunteer.phoneNumber, Validators.compose([Validators.required, Validators.minLength(2), Validators.pattern(regExAge)])],
+    'sexCtrl': [this.currentTempVolunteer.sex, Validators.required],
+    'partyAffiliationCtrl': [this.currentTempVolunteer.partyAffiliation, Validators.required],
+    'shiftsCtrl': [this.currentTempVolunteer.shifts, Validators.required],
+    'passcodeCtrl': [this.currentTempVolunteer.passcode, Validators.required],
 
-        });
+    });
 
-        /* this.changeForm.reset('fullNameCtrl');
-        this.changeForm.get('fullNameCtrl');
-        this.changeForm.patchValue('fullNameCtrl');
-        */
-        
-        //end form stuff
-        
-        //setVolunteer to be erased
-        volunteerservice.setNewVolunteer(this.currentTempVolunteer);
-        
+    //this.changeForm.reset(); ('fullNameCtrl');
+    //this.changeForm.get('fullNameCtrl');
+    //this.changeForm.patchValue('fullNameCtrl');
 
-        // get exposed value
-        if(volunteerservice.currentVolunteer!==null){
-            this.exposedYesOrNo = this.volunteerservice.isEmailExposed(this.currentTempVolunteer);
-        }
+  
+  //end form stuff
+      
+      //setVolunteer to be erased
+      volunteerservice.setNewVolunteer(this.currentTempVolunteer);
+      
 
-
-        //get shift printout
-        this.printedShifts = this.volunteerservice.printShifts(this.currentTempVolunteer);
-
-        this.thisTempStation = this.pollingstationservice.getPollingStationbyKey(this.currentTempVolunteer.associatedPollingStationKey)
-        this.thisTempStationPrecint = this.thisTempStation.precinctNumber;
-
-        //get associate volunteer keys
-        this.fullVolunteerKeyList = this.pollingstationservice.getAssociatedVolunteerKeyList(this.currentTempVolunteer.associatedPollingStationKey);
-
-        //make array of associated volunteerservices
-        this.fullVolunteerList = this.volunteerservice.getVolunteerArrayByKeyList(this.fullVolunteerKeyList);
-
-        //end constructor
-    }
-
-    /*  onchangeName(fullNameCtrl){
-        this.currentTempVolunteer.fullName = this.changeForm.form;
-        console.log(fullNameCtrl.value);
-        }
-    */
+      // get exposed value
+      if(volunteerservice.currentVolunteer!==null){
+      this.exposedYesOrNo = this.volunteerservice.isEmailExposed(this.currentTempVolunteer);
+      }
 
 
-    askExpose(){
-        let confirm = this.alertCtrl.create({
-            title: 'Would you like to expose your email address to your team?',
-            message: 'This will help you organize with each other before and on election day. You can change this setting later in your account.',
-            buttons: [
-                {
+      //get shift printout
+      this.printedShifts = this.volunteerservice.printShifts(this.currentTempVolunteer);
+
+      this.thisTempStation = this.pollingstationservice.getPollingStationbyKey(this.currentTempVolunteer.associatedPollingStationKey)
+      this.thisTempStationPrecint = this.thisTempStation.precinctNumber;
+
+      //get associate volunteer keys
+      this.fullVolunteerKeyList = this.pollingstationservice.getAssociatedVolunteerKeyList(this.currentTempVolunteer.associatedPollingStationKey);
+
+      //make array of associated volunteerservices
+      this.fullVolunteerList = this.volunteerservice.getVolunteerArrayByKeyList(this.fullVolunteerKeyList);
+
+//end constructor
+  }
+
+/*  onchangeName(fullNameCtrl){
+  this.currentTempVolunteer.fullName = this.changeForm.form;
+  console.log(fullNameCtrl.value);
+  }
+*/
+
+
+  askExpose(){
+     let confirm = this.alertCtrl.create({
+                title: 'Would you like to expose your email address to your team?',
+                message: 'This will help you organize with each other before and on election day. You can change this setting later in your account.',
+                buttons: [
+                    {
                     text: 'No',
                     handler: () => {
                         this.currentTempVolunteer.exposeEmail = false;
@@ -203,10 +204,11 @@ export class TempPage {
     }
 
 
-    askSex(){
-        this.volunteerservice.printVolunteerKeysFromList()
-        this.presentConfirm();
-    }
+
+askSex(){
+  console.log(this.currentTempVolunteer.fullName);
+  this.presentConfirm();
+}
 
     askShifts(){
         let confirm = this.alertCtrl.create({
