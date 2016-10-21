f89ca9b1eeb77f76969dfb4ece3a73b94c72abcc
Melissa Schwartz <melissjs@gmail.com>
2016-10-20 22:57:04 -0700

    merging

--- app/pages/unregisteredsignin/unregisteredsignin.ts
+++ app/pages/unregisteredsignin/unregisteredsignin.ts
@@ -6,13 +6,16 @@ import {RegistrationsuccessPage} from '../registrationsuccess/registrationsucces
 import { Volunteer} from '../../volunteer';
 import { Volunteerservice } from '../../providers/volunteerservice/volunteerservice'
 //import { VOLUNTEERS} from '../../volunteerlist.ts';
+import {RestService} from '../../providers/rest-service/rest-service';
 
 import * as globals from '../../globals';
 
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
+
 @Component({
-  templateUrl: 'build/pages/unregisteredsignin/unregisteredsignin.html',
+    templateUrl: 'build/pages/unregisteredsignin/unregisteredsignin.html',
+    providers: [RestService]
 })
 export class UnregisteredsigninPage {
 newVolunteer: Volunteer;
@@ -40,10 +43,11 @@ volunteers: Volunteer[];
 registerForm: FormGroup;
 dbSex: string;
 dbPartyAffiliation: string;
+properties: any;
 
 
 
-  constructor(private navCtrl: NavController, private alertCtrl: AlertController, public fb: FormBuilder, volunteerservice: Volunteerservice) {
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController, public fb: FormBuilder, private restSvc: RestService,  volunteerservice: Volunteerservice) {
   this.navCtrl = navCtrl;
   this.newVolunteer = null;
   this.volunteerKey = null;
@@ -66,6 +70,8 @@ dbPartyAffiliation: string;
   this.enterOtherPartyAffiliation = null;
   this.volunteerservice = volunteerservice;
   this.volunteers = this.volunteerservice.getVolunteers();
+  this.restSvc = restSvc;
+  this.properties = null;
 
   
         //form stuff
@@ -114,33 +120,127 @@ dbPartyAffiliation: string;
 }
 
 
-        askToExposeEmail(){
 
-                    
-                let confirm = this.alertCtrl.create({
-                title: 'Would you like to expose your email address to your team?',
-                message: 'This will help you organize with each other before and on election day. You can change this setting later in your account.',
-                buttons: [
-                    {
+
+
+    onChangeFullName(value){
+        this.enterFullName = value;
+    }
+
+    onChangeEmail(value){
+        this.enterEmailAddress = value;
+
+    }
+
+    askToExposeEmail(){
+
+        
+        let confirm = this.alertCtrl.create({
+            title: 'Would you like to expose your email address to your team?',
+            message: 'This will help you organize with each other before and on election day. You can change this setting later in your account.',
+            buttons: [
+                {
                     text: 'No',
                     handler: () => {
                         this.enterExposeEmail = false;
                         console.log('Disagree clicked');
                     }
-                    },
-                    {
+                },
+                {
                     text: 'Yes',
                     handler: () => {
                         this.enterExposeEmail = true;
                         console.log('Agree clicked' + this.enterExposeEmail);
                         
                     }
-                    }
-                ]
-                });
-                confirm.present();
-        }
 
+                }
+            ]
+        });
+        confirm.present();
+        
+        
+        /*if (this.enterEmailAddress !== null){
+          let alert = this.alertCtrl.create({
+          title: 'Expose Email to Team?',
+          subTitle: 'Congratulations you have successfully registered to become an auditor! Thank you for your participation. Now Please read the next page and choose your polling location and shift(s).',
+          buttons: ['OK'] 
+          });
+          alert.present();}*/
+    }
+
+
+    onChangePhoneNumber(value){
+        this.enterPhoneNumber = value;
+    }
+
+    onChangeAge(value){
+        this.enterAge = value;
+    }
+
+    onChangeSex(value){
+        this.enterSex = value;
+    }
+
+    onChangePartyAffiliationFromList(value){
+        this.enterPartyAffiliationFromList = value;
+        /*if (this.enterPartyAffiliationFromList!=="other"){
+          this.enterOtherPartyAffiliation=null;
+          this.enterPartyAffiliation=this.enterPartyAffiliationFromList;
+          }*/
+    }
+
+    onChangeOtherPartyAffiliation(value){
+        this.enterOtherPartyAffiliation = value;
+        /*if (this.enterPartyAffiliationFromList=="other" && this.enterOtherPartyAffiliation!==null){
+          this.enterPartyAffiliation=this.enterOtherPartyAffiliation;
+          }*/
+    }
+
+    onChangePasscode1(value){
+        this.enterPasscode1 = value;
+    }
+
+    onChangePasscode2(value){
+        this.enterPasscode2 = value;
+        if (this.enterPasscode1==this.enterPasscode2){
+            this.enterPasscode=this.enterPasscode1;
+        }
+    }
+
+    presentVerificationInit() {
+        let alertpvi = this.alertCtrl.create({
+            title: 'Verify Phone Number',
+            subTitle: 'How would you like to be sent the code',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: data => {
+                        console.log('Cancel clicked.. nothing to do');
+                    }
+                },
+                {
+                    text: 'Voice Call',
+                    handler: data => {
+                        let navTransition = alertpvi.dismiss();
+                        this.verifyPhoneNumber('call');
+                    }
+                },
+                {
+                    text: 'SMS Text',
+                    handler: data => {
+                        let navTransition = alertpvi.dismiss();
+                        this.verifyPhoneNumber('sms');
+                    }
+                }
+            ]
+        });
+        //timeout the error to let other modals finish dismissing.
+        setTimeout(()=>{
+            alertpvi.present();
+        },500);
+    }
 
 
 
@@ -159,6 +259,73 @@ dbPartyAffiliation: string;
                 pcalert.present();
                 return;
             }
+
+
+                    // ERICS PART OF ON SUBMIT
+        if ((this.newVolunteer.fullName == null) || /*
+            (this.newVolunteer.emailAddress == null) || */
+            (this.newVolunteer.phoneNumber == null) /* ||
+            (this.newVolunteer.passCode == null) ||      
+            (this.newVolunteer.passCheck == null) ||
+            (this.newVolunteer.enterShift == null) */ ) {
+            var reqFields = "";
+            var first = true;
+            /* if (this.newVolunteer.fullName == null) {
+                reqFields += "Full Name";
+                first = false;
+            }
+            if (this.newVolunteer.emailAddress == null) {
+                if (!first) {
+                    reqFields += ", ";
+                }
+                reqFields += "Email Address";
+                first = false;
+            } */
+            if (this.newVolunteer.phoneNumber == null) {
+                if (!first) {
+                    reqFields += ", ";
+                }
+                reqFields += "Cell Phone";
+                first = false;
+            } /*
+            if (this.newVolunteer.passCode == null) {
+                if (!first) {
+                    reqFields += ", ";
+                }
+                reqFields += "Passcode";
+                first = false;
+            }
+            if (this.newVolunteer.passCheck == null) {
+                if (!first) {
+                    reqFields += ", ";
+                }
+                reqFields += "Re-entered Passcode";
+                first = false;
+            }
+            */
+
+            let alert = this.alertCtrl.create({
+                title: reqFields + ' field(s) required.',
+                subTitle: 'Please fill in all required fields.',
+                buttons: ['OK']
+            });
+            alert.present();
+        } else {
+
+            /* if (that.passCode != that.passCheck) {
+                let alert = this.alertCtrl.create({
+                    title: 'Passcodes do not match',
+                    subTitle: 'Please re-enter passcodes.',
+                    buttons: ['OK']
+                });
+                alert.present();
+            } else */ {
+
+                this.presentVerificationInit();
+            }
+        }
+
+        //END ERICS ONSUBMITCODE
 
 
 
@@ -181,6 +348,242 @@ dbPartyAffiliation: string;
             //this.currentTempVolunteer.partyAffiliation = value.partyAffiliationCtrl;
 
         
+    presentVerificationCheck(subtitle:string) {
+        var that = this;
+        let alertpvc = this.alertCtrl.create({
+            title: 'Verify Code',
+            subTitle: subtitle,
+            inputs: [
+                {
+                    name: 'vcode',
+                    placeholder: '1234'
+                }
+            ],
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    handler: data => {
+                        alertpvc.dismiss();
+                        console.log('Cancel clicked.. nothing to do');
+                    }
+                },
+                {
+                    text: 'OK',
+                    role: 'submit',
+                    handler: data => {
+                        console.log('data=' + data.vcode);
+                        if ((data.vcode != null) &&
+                            (data.vcode.match("[0-9]{4}"))) {
+                            alertpvc.dismiss();
+                            that.verifyCode(data.vcode);
+                        } else {
+                            alertpvc.dismiss();
+                            this.presentVerificationCheck('Invalid Code Entered, Try Again...');
+                        }
+                    }
+                }
+            ]
+        });
+        //timeout the error to let other modals finish dismissing.
+        setTimeout(()=>{
+            alertpvc.present();
+        },500);
+    }
+
+    verifyPhoneNumber(via: string) {
+        var that = this;
+        try {
+            that.restSvc.sendAuthyRequest(via,that.newVolunteer.phoneNumber)
+                .subscribe( data => {
+                    that.properties = data;
+                    console.log('successful call:' + that.properties);
+                    if (that.properties.success) {
+                        this.presentVerificationCheck('Enter the 4 digit code');
+                    } else {
+                        let alert = that.alertCtrl.create({
+                            title: 'Error Verifying Phone',
+                            subTitle: that.properties.message,
+                            buttons: [{
+                                text: 'OK',
+                                handler: () => {
+                                    alert.dismiss();
+                                }
+                            }]
+                        });
+                        //timeout the error to let other modals finish dismissing.
+                        setTimeout(()=>{
+                            alert.present();
+                        },500);
+                    }
+                }, err => {
+                    console.log('error occurred ' + err.toString());
+                    if ((err.toString().startsWith("Response with status: 0")) ||
+                        (err.toString().startsWith("Response with status: 404"))) {
+                        that.properties = "Unknown Error!";
+                        this.testError();
+                        return;
+                    } else {
+                        that.properties = err.toString();
+                    }
+                    // console.log(error.stack());
+                    let alert = that.alertCtrl.create({
+                        title: 'Error Verifying Phone',
+                        subTitle: that.properties,
+                        buttons: [{
+                            text: 'OK',
+                            handler: () => {
+                                alert.dismiss();
+                            }
+                        }]
+                    });
+                    //timeout the error to let other modals finish dismissing.
+                    setTimeout(()=>{
+                        alert.present();
+                    },500);
+                }, () => {console.log('registration complete')});
+        } catch (err) {
+            console.error(err);
+            console.log('error in Submitting, exc='+ err.toString())
+            let alert2 = that.alertCtrl.create({
+                title: 'Error Authorizing',
+                subTitle: 'There was a problem sending '
+                    + 'your token - sorry :(' + err.toString(),
+                buttons: ['OK']
+            });
+            alert2.present();
+        }
+        
+    }
+
+    verifyCode(code: string) {
+        var that = this;
+        try {
+            that.restSvc.sendAuthyVerify(that.newVolunteer.phoneNumber, code)
+                .subscribe( data => {
+                    that.properties = data;
+                    console.log('successful call:' + that.properties);
+                    if (that.properties.success) {
+                        this.sendVerificationEmail();
+                    } else {
+                        this.presentVerificationCheck('Incorrect Code Entered, Try Again...');
+                    }
+                }, err => {
+                    console.log('error occurred ' + err.toString());
+                    if (err.toString().startsWith("Response with status: 0")) {
+                        that.properties = "Unknown Error!";
+                    } else {
+                        that.properties = err.toString();
+                    }
+                    // console.log(error.stack());
+                    let alert = that.alertCtrl.create({
+                        title: 'Error Verifying Phone',
+                        subTitle: that.properties,
+                        buttons: [{
+                            text: 'OK',
+                            handler: () => {
+                                alert.dismiss();
+                            }
+                        }]
+                    });
+                    //timeout the error to let other modals finish dismissing.
+                    setTimeout(()=>{
+                        alert.present();
+                    },500);
+                }, () => {console.log('registration complete')});
+        } catch (err) {
+            console.error(err);
+            console.log('error in Submitting, exc='+ err.toString())
+            let alert2 = that.alertCtrl.create({
+                title: 'Error Authorizing',
+                subTitle: 'There was a problem sending '
+                    + 'your token - sorry :(' + err.toString(),
+                buttons: ['OK']
+            });
+            alert2.present();
+        }
+    }
+
+    successForward(real:boolean) {
+        var subtitle;
+        var that = this;
+        if (real) {
+            subtitle = 'You should shortly receive an email.  Please click on the link in the email to verify your email address';
+        } else {
+            subtitle = 'For TESTING PURPOSES, we simulate success here.  You would expect to receive an email.  and be told to Please click on the link in the email to verify your email address';
+        }
+        let alert = that.alertCtrl.create({
+            title: 'Verification Successful',
+            /* subTitle: 'Congratulations you have successfully registered to become an auditor! Thank you for your participation. Now Please read the next page and choose your polling location and shift(s).', */
+            subTitle: subtitle,
+            buttons: [{
+                text: 'OK',
+                handler: () => {
+                    alert.dismiss();
+                }
+            }]
+        });
+        //timeout the error to let other modals finish dismissing.
+        setTimeout(()=>{
+            alert.present();
+        },500);
+        // Send to token verification page
+        that.navCtrl.push(RegistrationsuccessPage, {
+        });
+    }
+
+    sendVerificationEmail() {
+        var that = this;
+        try {
+            that.restSvc.registerUser(that.newVolunteer)
+                .subscribe( data => {
+                    that.properties = data;
+                    // Expect response created here...
+                    console.log('successful call:' + that.properties);
+                    this.successForward(true);
+                }, err => {
+                    console.log('error occurred ' + err.toString());
+                    if ((err.toString().startsWith("Response with status: 0")) ||
+                        (err.toString().startsWith("Response with status: 404"))) {
+                        that.properties = "Unknown Error!";
+                        this.testError();
+                        return;
+                    } else {
+                        that.properties = err.toString();
+                    }
+                    // console.log(error.stack());
+                    let alert = that.alertCtrl.create({
+                        title: 'Error Registering Account',
+                        subTitle: that.properties,
+                        buttons: [{
+                            text: 'OK',
+                            handler: () => {
+                                alert.dismiss();
+                            }
+                        }]
+                    });
+                    //timeout the error to let other modals finish dismissing.
+                    setTimeout(()=>{
+                        alert.present();
+                    },500);
+                }, () => {console.log('registration(register) complete')});
+        } catch (err) {
+            console.error(err);
+            console.log('error in Submitting, exc='+ err.toString())
+            let alert2 = that.alertCtrl.create({
+                title: 'Error Authorizing',
+                subTitle: 'There was a problem sending '
+                    + 'your token - sorry :(' + err.toString(),
+                buttons: ['OK']
+            });
+            alert2.present();
+        }
+    }
+
+    testError() {
+        // Only happens when we don't actually have a real server to talk to..
+        this.successForward(false);
+    }
 
             
             this.volunteerservice.printVolunteer(this.newVolunteer);
@@ -240,3 +643,4 @@ dbPartyAffiliation: string;
 
     }
 }
+
---
+++ app/providers/rest-service/rest-service.ts
@@ -0,0 +1,143 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import * as config from '../../config';
+import { Volunteer} from '../../volunteer';
+
+let authyURL = config.AUTHY_VER_URL;
+
+/*
+  Generated class for the RestService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class RestService {
+
+    jsessionid: string;
+    csrf_token: string;
+
+    constructor(private http: Http) {
+        // generate values
+        this.jsessionid = null;// this.generateUUID();
+        this.csrf_token = null;// this.generateUUID();
+    }
+
+    generateUUID(){
+        var d = new Date().getTime();
+        if(window.performance && typeof window.performance.now === "function"){
+            d += performance.now(); //use high-precision timer if available
+        }
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    }
+
+    sendAuthyRequest(via: string, cellPhone: string) {
+
+	// let options = new RequestOptions({ headers: null, withCredentials: true});
+        var url = config.AUTHY_VER_URL + cellPhone + '?via=' + via;
+
+        // var retval1 = this.http.post(url, params, { headers: headers });
+        var retval1 = this.http.get(url);
+        
+        // body, options
+	var that = this;
+        /* var retval1b = retval1.subscribe((res) => {
+            that.csrf_token = res.headers.get('CSRF-TOKEN');
+        });*/
+
+        var retval2 = retval1.map(
+            res => res.json()
+        );
+        return retval2;
+            // .catch(this.handleError);
+    }
+    
+    sendAuthyVerify(cellPhone: string, code: string) {
+
+        // let options = new RequestOptions({ headers: headers });
+        var url = config.AUTHY_CHK_URL + cellPhone + '/' + code;
+
+        // var retval1 = this.http.post(url, params, { headers: headers });
+        var retval1 = this.http.get(url);
+        
+        // body, options
+
+        var retval2 = retval1.map(
+            res => res.json()
+        );
+        return retval2;
+            // .catch(this.handleError);
+    }
+    
+    cacheBuster() {
+        var now = new Date().getTime();
+        var retval = '?cacheBuster=' + now;
+        return retval;
+    }
+
+    registerUser(nv: Volunteer) {
+        var blankIdx = nv.fullName.indexOf(" ");
+        var firstName = null;
+        var lastName = null;
+        if (blankIdx < 1) {
+            // no last name specified..
+            firstName = nv.fullName;
+        } else {
+            firstName = nv.fullName.substring(0,blankIdx);
+            lastName = nv.fullName.substring(blankIdx+1);
+        }
+        var property = 
+            { "login": nv.phoneNumber, "firstName": firstName, "lastName": lastName,
+              "email": nv.emailAddress, "password": nv.passcode, "langKey": "en" };
+        var json = JSON.stringify(property);
+        var params = /* 'json=' +  */ json;
+        let headers = new Headers();
+        headers.append('Accept', 'application/json, text/plain, */*');
+        if (this.csrf_token != null) {
+            headers.append('X-CSRF-TOKEN', this.csrf_token);
+        }
+        headers.append('Content-Type', 'application/json;charset=UTF-8');
+	let options = new RequestOptions({ headers: headers, withCredentials: true});
+
+        var url = config.MT_HOST + '/api/register' + this.cacheBuster();
+        var retval1 = this.http.post(url, params, options);
+        // body, options
+        var retval2 = retval1.map(res => res.json());
+        return retval2;
+    }
+
+    handleError(error) {
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error' + error.toString());
+    }
+
+    anonSignin() {
+        var property = 
+            { "withCredentials": "true", "j_username": "anon", "j_password": "anon", "remember-me" : "true"
+             };
+        var json = JSON.stringify(property);
+        var params = /* 'json=' +  */ json;
+        let headers = new Headers();
+        /* headers.append('Access-Control-Allow-Origin', '*');
+        headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        */
+        headers.append('Content-Type', 'application/json');
+
+        // let options = new RequestOptions({ headers: headers });
+        var url = config.MT_HOST + '/api/authentication';
+        var retval1 = this.http.post(url, params, { headers: headers })
+        
+        // body, options
+
+        var retval2 = retval1.map(res => res.json());
+        return retval2;
+    }
+}
